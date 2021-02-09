cf91c52b5f57eeece06b2aa5b0867fdf18779f04
Abdullah Numan <anewman15@hotmail.com>
2021-02-09 20:23:21 +0600

    Remove img markup for unused logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>
           Calculator App using React
         </h1>
