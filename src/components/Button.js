3d7cd63408300d22944f19c3ce6914e483c1bb3e
Abdullah Numan <anewman15@hotmail.com>
2021-02-09 21:46:31 +0600

    Create Button component

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Button extends React.Component {
+  render() {
+    return (
+      <button>
+        {this.props.btnValue}
+      </button>
+    );
+  }
+}
