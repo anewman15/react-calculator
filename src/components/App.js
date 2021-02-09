4ad4cf8d44c5803f246730f7aaeb665033a6fc2e
Abdullah Numan <anewman15@hotmail.com>
2021-02-09 21:16:44 +0600

    Modify App.js component to add Display and ButtonPanel components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
+import React from 'react';
 import '../styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Calculator App using React
-        </h1>
-      </header>
-    </div>
+    <React.Fragment>
+      <Display />
+      <ButtonPanel />
+    </React.Fragment>
   );
 }
 
