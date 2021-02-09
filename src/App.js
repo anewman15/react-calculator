7db25a351ea94a58a7eb1a99ad10e5eb35f6cac1
Abdullah Numan <anewman15@hotmail.com>
2021-02-09 20:22:26 +0600

    Remove log and add app heading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,17 +5,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Calculator App using React
+        </h1>
       </header>
     </div>
   );
