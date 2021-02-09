896a5d5d0c9063dabc977ecef4cc52066686ab47
Abdullah Numan <anewman15@hotmail.com>
2021-02-09 20:15:45 +0600

    Change eslintrc.json file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('app'),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('app'),
 );
 reportWebVitals();
