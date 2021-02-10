78548a1c252908987ab2af8302d3b8be162cfae0
Abdullah Numan <anewman15@hotmail.com>
2021-02-10 18:30:16 +0600

    Make button array multidimensional, display buttons in groups

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -6,10 +6,19 @@ import React from 'react';
 import Button from './Button';
 
 const ButtonPanel = props => {
-  const btnNames = ['AC', '+/-', '%', '/', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-  const keyButtons = btnNames.map(name => (
-    <div key={name.toString()}>
-      <Button btnName={name} />
+  const btnGroups = [
+    ['AC', '+/-', '%', '/'],
+    ['7', '8', '9', 'X'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '.', '='],
+  ];
+
+  const keyButtons = btnGroups.map(group => (
+    <div key={group.toString()}>
+      {group.map(name => (
+        <Button key={name.toString()} btnName={name} />
+      ))}
     </div>
   ));
 
