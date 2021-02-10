0031d20ec588bd5f2b6934f53c4a824a1d6c5390
Abdullah Numan <anewman15@hotmail.com>
2021-02-10 18:07:57 +0600

    Add propType to btnName in Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,11 +3,16 @@
 */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Button = props => (
   <button>
     {props.btnName}
   </button>
 );
+
+Button.propTypes = {
+  btnName: PropTypes.string.isRequired,
+};
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -8,9 +8,9 @@ import Button from './Button';
 const ButtonPanel = props => {
   const btnNames = ['AC', '+/-', '%', '/', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
   const keyButtons = btnNames.map(name => (
-  <div key={name.toString()}>
-    <Button btnName={name} />
-  </div>
+    <div key={name.toString()}>
+      <Button btnName={name} />
+    </div>
   ));
 
   return (
