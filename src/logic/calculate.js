dc67bdeb038085ecf9a7c3575ddcea85891828ee
Abdullah Numan <anewman15@hotmail.com>
2021-02-10 20:20:23 +0600

    Create calculate function

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,2 +1,19 @@
 import Big from 'big.js';
 
+const calculate = (calculatorData, btnName) => {
+  const data = { ...calculatorData };
+  switch (btnName) {
+    case '+/-':
+      data.total *= -1;
+      data.next *= -1;
+      return data;
+    case '%':
+      data.total *= 100;
+      data.next *= 100;
+      return data;
+    default:
+      return data;
+  }
+};
+
+export default calculate;
