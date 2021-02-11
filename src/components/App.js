b62bef5ad9f265be5da1f3a5ba2fe75ae14bfab8
Abdullah Numan <anewman15@hotmail.com>
2021-02-11 14:21:30 +0600

    Change App to class component

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,17 @@ import ButtonPanel from './ButtonPanel';
 import { calculate } from '../logic/calculate';
 import '../styles/App.css';
 
-const App = props => (
-  <div>
-    <Display calcResult={props.result} />
-    <ButtonPanel />
-  </div>
-);
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Display calcResult={this.props.result} />
+        <ButtonPanel />
+      </div>
+    );
+  }
+}
 
 App.defaultProps = {
   result: '0',
 };
-
-export default App;
