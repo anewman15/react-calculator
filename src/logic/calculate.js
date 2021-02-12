b027150c7145b6c715785fcc70453c6a5529df5b
Abdullah Numan <anewman15@hotmail.com>
2021-02-12 16:41:07 +0600

    Make calculate function concatenate digits

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,14 +2,43 @@ import operate from './operate';
 
 const calculate = (calculatorData, btnName) => {
   const data = { ...calculatorData };
+  const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  // const operators = ['%', '/', 'X', '+', '-'];
+
+  // let operator;
+  let digit;
+  if (digits.includes(btnName)) {
+    digit = btnName;
+  }
+
+  // if (operators.includes(btnName)) {
+  //   operator = btnName;
+  // }
+
   switch (btnName) {
+    case 'AC':
+      data.total = 0;
+      data.next = 0;
+      data.operation = null;
+      return data;
     case '+/-':
       data.total *= -1;
       data.next *= -1;
+      data.operation = null;
       return data;
-    case data.operation:
-      data.total = operate(data.total, data.next, data.operation);
+    case digit:
+      data.total = data.total ? data.total + digit : digit;
+      if (data.total && data.operation) {
+        data.next = data.next ? data.next + digit : digit;
+      }
       return data;
+    // case '.':
+    //   data.total = data.total.includes('.') ? `${data.total}` : `${data.total}${'.'}`;
+    //   return data;
+    // case operator:
+    //   data.operation = operator;
+    //   data.next = null;
+    //   return data;
     default:
       return data;
   }
