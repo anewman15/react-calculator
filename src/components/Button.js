1d588d146c6b3cb9806e7afa896cfd0a84fa06cb
Abdullah Numan <anewman15@hotmail.com>
2021-02-17 15:40:38 +0600

    Apply styles logic for buttons using btnBgColor function

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,15 +5,16 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { HandleClickContext } from './App';
+import btnBgColor from '../logic/style';
 
 const Button = props => {
+  const { btnName } = props;
+  const handleClick = useContext(HandleClickContext);
   const zeroBtnWidth = {
     width: '50%',
   };
-  const { btnName } = props;
-  const handleClick = useContext(HandleClickContext);
   return (
-    <button className='btn' style={btnName === '0' ? zeroBtnWidth : null} onClick={() => handleClick(btnName)}>
+    <button className={`btn ${btnBgColor(btnName)}`} style={btnName === '0' ? zeroBtnWidth : null} onClick={() => handleClick(btnName)}>
       {btnName}
     </button>
   );
