fe199be68a996e5de9d8d82814b0f6c581403e16
Abdullah Numan <anewman15@hotmail.com>
2021-02-19 20:55:51 +0600

    Create snapshot test for Calculator

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -1,5 +1,10 @@
+/*
+ eslint-disable no-unused-vars, import/named, import/no-cycle
+*/
+
 import React from 'react';
-import { render, act, unmountComponentAtNode } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
+import { unmountComponentAtNode } from 'react-dom';
 import pretty from 'pretty';
 import Calculator from './Calculator';
 
@@ -13,4 +18,13 @@ afterEach(() => {
   unmountComponentAtNode(container);
   container.remove();
   container = null;
+});
+
+it('should render a display and button panel', () => {
+  act(() => {
+    render(<Calculator />, container);
+
+    expect(pretty(container.innerHTML))
+      .toMatchSnapshot();
+  });
 });
