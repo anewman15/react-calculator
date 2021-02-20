6a2404a691fc169f831bb875a0af7452ea180d1c
Abdullah Numan <anewman15@hotmail.com>
2021-02-20 21:44:27 +0600

    Render app using Main instead of Routes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
-import Routes from './components/Routes';
+import Main from './components/Main';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <Main />
   </React.StrictMode>,
   document.getElementById('app'),
 );
