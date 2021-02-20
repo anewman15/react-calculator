75af8b8f8fab6def2056032b5b0a61c65e38d9d0
Abdullah Numan <anewman15@hotmail.com>
2021-02-20 21:15:40 +0600

    Create snapshot test for TopNav component

---
+++ src/components/TopBav.test.js
@@ -0,0 +1,31 @@
+/*
+ eslint-disable no-unused-vars, import/named, import/no-cycle
+*/
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { unmountComponentAtNode } from 'react-dom';
+import TopNav from './TopNav';
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe('TopNav', () => {
+  it('should render a navigation bar at the top of the site', () => {
+    const tree = renderer
+      .create(<TopNav />)
+      .toJSON();
+
+    expect(tree)
+      .toMatchSnapshot();
+  });
+});
