4713489552cfd499574708860af29fa8b833e049
Abdullah Numan <anewman15@hotmail.com>
2021-02-21 22:29:47 +0600

    Add test for . (decimal) in calculate.js

--- src/__test__/__logic__/calculate.test.js
+++ src/__test__/__logic__/calculate.test.js
@@ -25,4 +25,16 @@ describe('Calculate function', () => {
     const calcValue = calculate(calculatorData, '%');
     expect(calcValue.display).toBe('2.5');
   });
+
+  it('if decimal not present, returns a display with decimal appended to the total when btnName is .', () => {
+    calculatorData.total = '250';
+    const calcValue = calculate(calculatorData, '.');
+    expect(calcValue.display).toBe('250.');
+  });
+
+  it('if decimal already exists returns a display with no decimal appended to the total when btnName is .', () => {
+    calculatorData.total = '250';
+    const calcValue = calculate(calculatorData, '.');
+    expect(calcValue.display).toBe('250.');
+  });
 });
