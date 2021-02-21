993404d4e3bafff0cbc687bd6a741c190c9fdc4c
Abdullah Numan <anewman15@hotmail.com>
2021-02-21 20:56:58 +0600

    Write more tests for operate.js

--- src/__test__/__logic__/operate.test.js
+++ src/__test__/__logic__/operate.test.js
@@ -12,4 +12,28 @@ describe('Operate function', () => {
 
     expect(calcValue).toBe('Undefined');
   });
+
+  it('returns a value for multiplication when the operator is X', () => {
+    const calcValue = operate('18', '4.5', 'X');
+
+    expect(calcValue).toBe(81);
+  });
+
+  it('returns a value for addition when the operator is +', () => {
+    const calcValue = operate('18', '4.5', '+');
+
+    expect(calcValue).toBe(22.5);
+  });
+
+  it('returns a value for subtraction when the operator is -', () => {
+    const calcValue = operate('18', '4.5', '-');
+
+    expect(calcValue).toBe(13.5);
+  });
+
+  it('returns Undefined when the operator is anything else', () => {
+    const calcValue = operate('18', '4.5', 'a');
+
+    expect(calcValue).toBe(undefined);
+  });
 });
